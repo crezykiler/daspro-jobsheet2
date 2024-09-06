2e3fa0ca26313d99a06c7433e058a4b894d2f77a
crezykiler <kiminsetyawan@gmail.com>
2024-09-06 13:57:52 +0700

    simpan kode java ke github jio

---
+++ PengenalanDiri17.java
@@ -0,0 +1,9 @@
+public class MyFirstJavaGithub17 {
+
+    public static void main(String [] args){
+        System.out.println("Nama    = Rachmad Zaki Setyawan");
+        System.out.println("NIM     = 244107060107");
+        System.out.println("Kelas   = SIB1A");
+    }
+
+}
